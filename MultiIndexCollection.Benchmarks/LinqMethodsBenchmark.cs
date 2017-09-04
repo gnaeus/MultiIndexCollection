3801e2a011f9ed10bc3add675c97c069c153e385
PanyushkinD <PanyushkinD@quantumart.ru>
2017-09-04 09:46:33 +0300

    Add benchmarks fro Linq methods

--- MultiIndexCollection.Benchmarks/LinqMethodsBenchmark.cs
+++ MultiIndexCollection.Benchmarks/LinqMethodsBenchmark.cs
@@ -13,17 +13,23 @@ namespace MultiIndexCollection.Benchmarks
             public int Property { get; set; }
         }
 
-        [Params(100, 1000, 10000, 100000)]
+        [Params(100, 300, 1000, 3000, 10000, 30000, 100000, 300000, 1000000)]
         public int Length { get; set; }
 
         [Params(3)]
         public int MaxDuplicates { get; set; }
 
         private IList<Entity> _outer;
-        private IList<Entity> _inner;
+        private IList<Entity> _list;
 
-        private IndexedCollection<Entity> _hashIndex;
-        private IndexedCollection<Entity> _sortedIndex;
+        private IndexedCollection<Entity> _hashIndexed;
+        private IndexedCollection<Entity> _sortedIndexed;
+
+        private ILookup<int, Entity> _lookup;
+        private ILookup<int, Entity> _hashLookup;
+        private ILookup<int, Entity> _sortedLookup;
+
+        private int _maxNumber = 0;
 
         [GlobalSetup]
         public void GlobalSetup()
@@ -42,48 +48,53 @@ namespace MultiIndexCollection.Benchmarks
 
             _outer.Shuffle();
 
-            _inner = new List<Entity>(Length);
+            _list = new List<Entity>(Length);
             
-            for (int i = 0; i < Length && _inner.Count < Length; i++)
+            for (int i = 0; i < Length && _list.Count < Length; i++)
             {
                 for (int j = 0; j < random.Next(MaxDuplicates + 1); j++)
                 {
-                    _inner.Add(new Entity { Property = i });
+                    _list.Add(new Entity { Property = i });
                 }
+                _maxNumber = i;
             }
 
-            _inner.Shuffle();
+            _list.Shuffle();
 
-            _hashIndex = _inner.IndexBy(e => e.Property);
+            _hashIndexed = _list.IndexBy(e => e.Property);
 
-            _sortedIndex = _inner.IndexBy(e => e.Property, true);
+            _sortedIndexed = _list.IndexBy(e => e.Property, true);
+
+            _lookup = _list.ToLookup(e => e.Property);
+
+            _hashLookup = _hashIndexed.ToLookup(e => e.Property);
+
+            _sortedLookup = _sortedIndexed.ToLookup(e => e.Property);
         }
 
         [Benchmark]
-        public int LinqJoin()
+        public int LinqGroupBy()
         {
-            return _outer
-                .Join(_inner,
-                    o => o.Property, i => i.Property,
-                    (o, i) => o.Property + i.Property)
-                .Sum();
+            return _list.GroupBy(e => e.Property).Sum(g => g.Key);
         }
 
         [Benchmark]
-        public int IndexedJoin()
+        public int HashIndexGroupBy()
         {
-            return _hashIndex
-                .Join(_outer,
-                    i => i.Property, o => o.Property,
-                    (i, o) => i.Property + o.Property)
-                .Sum();
+            return _hashIndexed.GroupBy(e => e.Property).Sum(g => g.Key);
+        }
+
+        [Benchmark]
+        public int SortedIndexGroupBy()
+        {
+            return _sortedIndexed.GroupBy(e => e.Property).Sum(g => g.Key);
         }
 
         [Benchmark]
         public int LinqGroupJoin()
         {
             return _outer
-                .GroupJoin(_inner,
+                .GroupJoin(_list,
                     o => o.Property, i => i.Property,
                     (o, i) => o.Property + i.Sum(e => e.Property))
                 .Sum();
@@ -92,7 +103,7 @@ namespace MultiIndexCollection.Benchmarks
         [Benchmark]
         public int IndexedGroupJoin()
         {
-            return _hashIndex
+            return _hashIndexed
                 .GroupJoin(_outer,
                     i => i.Property, o => o.Property,
                     (i, o) => i.Sum(e => e.Property) + o.Property)
@@ -100,27 +111,122 @@ namespace MultiIndexCollection.Benchmarks
         }
 
         [Benchmark]
+        public int LinqHavingMax()
+        {
+            int max = _list.Max(e => e.Property);
+
+            return _list.Where(e => e.Property == max).Sum(e => e.Property);
+        }
+
+        [Benchmark]
+        public int IndexedHavingMax()
+        {
+            return _sortedIndexed.HavingMax(e => e.Property).Sum(e => e.Property);
+        }
+
+        [Benchmark]
+        public int LinqHavingMin()
+        {
+            int min = _list.Min(e => e.Property);
+
+            return _list.Where(e => e.Property == min).Sum(e => e.Property);
+        }
+
+        [Benchmark]
+        public int IndexedHavingMin()
+        {
+            return _sortedIndexed.HavingMin(e => e.Property).Sum(e => e.Property);
+        }
+
+        [Benchmark]
+        public int LinqJoin()
+        {
+            return _outer
+                .Join(_list,
+                    o => o.Property, i => i.Property,
+                    (o, i) => o.Property + i.Property)
+                .Sum();
+        }
+
+        [Benchmark]
+        public int IndexedJoin()
+        {
+            return _hashIndexed
+                .Join(_outer,
+                    i => i.Property, o => o.Property,
+                    (i, o) => i.Property + o.Property)
+                .Sum();
+        }
+
+        [Benchmark]
+        public int LinqMax()
+        {
+            return _list.Min(e => e.Property);
+        }
+
+        [Benchmark]
+        public int IndexedMax()
+        {
+            return _sortedIndexed.Max(e => e.Property);
+        }
+
+        [Benchmark]
+        public int LinqMin()
+        {
+            return _list.Min(e => e.Property);
+        }
+
+        [Benchmark]
+        public int IndexedMin()
+        {
+            return _sortedIndexed.Min(e => e.Property);
+        }
+
+        [Benchmark]
         public int LinqOrderBy()
         {
-            return _inner.OrderBy(e => e.Property).Sum(e => e.Property);
+            return _list.OrderBy(e => e.Property).Sum(e => e.Property);
         }
 
         [Benchmark]
         public int IndexedOrderBy()
         {
-            return _sortedIndex.OrderBy(e => e.Property).Sum(e => e.Property);
+            return _sortedIndexed.OrderBy(e => e.Property).Sum(e => e.Property);
         }
 
         [Benchmark]
         public int LinqOrderByDescending()
         {
-            return _inner.OrderByDescending(e => e.Property).Sum(e => e.Property);
+            return _list.OrderByDescending(e => e.Property).Sum(e => e.Property);
         }
 
         [Benchmark]
         public int IndexedOrderByDescending()
         {
-            return _sortedIndex.OrderByDescending(e => e.Property).Sum(e => e.Property);
+            return _sortedIndexed.OrderByDescending(e => e.Property).Sum(e => e.Property);
+        }
+
+        private int _i = 0;
+
+        [Benchmark]
+        public bool LinqLookup()
+        {
+            _i = (_i + 1) % _maxNumber;
+            return _lookup.Contains(_i);
+        }
+
+        [Benchmark]
+        public bool HashIndexLookup()
+        {
+            _i = (_i + 1) % _maxNumber;
+            return _hashLookup.Contains(_i);
+        }
+
+        [Benchmark]
+        public bool SortedIndexLookup()
+        {
+            _i = (_i + 1) % _maxNumber;
+            return _sortedLookup.Contains(_i);
         }
     }
 }
